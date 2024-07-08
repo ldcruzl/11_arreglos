cde8deb1891335a5a29856709310c390ff92f322
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-08 03:08:57 -0500

    Actualizacion arreglo4: Se termino con la funcion principal y se pogramara la funcion

---
+++ arreglo4.cpp
@@ -0,0 +1,19 @@
+//CRUZ LLICA, Luis David
+
+#include<iostream>
+using namespace std;
+
+void relleno(int* arreglo, int tam, int mult);
+
+int main(){
+    int tam, mult;
+    cout<<"Ingrese el tamanio del arreglo: ";cin>>tam;
+    int* arreglo= new int[tam];
+    cout<<"Ingrese el numero a completar con multiplos: ";cin>>mult;
+    relleno(arreglo, tam, mult);
+    cout<<"Los multiplos son: ";
+    for(int i=0; i<tam; i++){
+        cout<<arreglo[i]<<" "<<endl;
+    }
+    return 0;
+}
